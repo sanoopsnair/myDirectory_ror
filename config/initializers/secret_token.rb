b4c4e568920747db3f154872e603cbecd15e5152
Sanoop S <sanoop@toobler.com>
2016-06-21 17:06:56 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyDirectoryRor::Application.config.secret_key_base = 'f8be444cc79c285222e876afbc9e87fc8b7c78c9b9cb856f15e7ab318eebd0ba9489f5a1dac5885a24416283eddb49776dab8714887ba1b0a00f8ac058dc444d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyDirectoryRor::Application.config.session_store :cookie_store, key: '_myDirectory_ror_session'
